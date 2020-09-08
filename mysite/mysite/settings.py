1340d6d05853c6b6c132fced55ac46f7261f1a3b
Chris <christophertberg@gmail.com>
2020-09-08 15:38:13 -0700

    build several views and templates

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '5%d=wi%!33&b501k&=^v$ec9=h8h4wf1(=dmjue(+r#$1#x@e%'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -2,6 +2,19 @@ from django.urls import path
 
 from . import views
 
+# Create an app_name to namespace our URLs in our templates.
+app_name = 'polls'
+
 urlpatterns = [
+    # ex: /polls/
     path('', views.index, name='index'),
+    # ex: /polls/5/
+    # using angle brackets captures part of the URL and sends it as a keyworrd argument to the view function.
+    # the <int: is a converter to determin what patterns should match.
+    # the :question_id> defines the name that will be used to identify the matched pattern.
+    path('<int:question_id>/', views.detail, name='detail'),
+    # ex: /polls/5/results/
+    path('<int:question_id>/results/', views.results, name='results'),
+    # ex: /polls/5/vote/
+    path('<int:question_id>/vote/', views.vote, name='vote')
 ]
